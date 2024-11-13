2893cae290312ab22f4e9814ee91b5b479ddd42e
Ian Ressa <ian@ianressa.xyz>
2024-11-12 21:05:12 -0500

    Actually finish implementing AbstractAddedValue

--- src/main/java/com/cifre/sap/su/goblinWeaver/weaver/addedValue/License.java
+++ src/main/java/com/cifre/sap/su/goblinWeaver/weaver/addedValue/License.java
@@ -9,26 +9,6 @@ import org.json.simple.parser.ParseException;
 import java.io.IOException;
 import java.util.*;
 
-public enum LicenseEnum {
-    agpl_v3,
-    apache_v2,
-    bsd_2,
-    bsd_3,
-    cddl_v1,
-    epl_only_v1,
-    epl_only_v2,
-    epl_v1,
-    epl_v2,
-    eupl_v1_1,
-    fdl_v1_3,
-    gpl_v1,
-    gpl_v2,
-    gpl_v3,
-    lgpl_v2_1,
-    lgpl_v3,
-    mit;
-}
-
 public class License extends AbstractAddedValue<LicenseEnum>{
 
     public License(String nodeId){
@@ -46,13 +26,28 @@ public class License extends AbstractAddedValue<LicenseEnum>{
     }
 
     @Override
-    public T getValue(){
+    public LicenseEnum getValue(){
         return value;
     }
 
     @Override
     public Map<String, Object> getValueMap(){
         return Collections.singletonMap(getAddedValueEnum().getJsonKey(), value);
+    }
+
+    @Override
+    public LicenseEnum stringToValue(String string){
+	return LicenseEnum.valueOf(string);
+    }
+
+    @Override
+    public void computeValue(){
+	this.value = LicenseEnum.agpl_v3;
+    }
+
+    @Override
+    public String valueToString(LicenseEnum value){
+	return value.name();
     }
 
     @Override
